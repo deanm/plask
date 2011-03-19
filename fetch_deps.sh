91646ca01eb0d05c3b32bb10a5bb38c1bc7f8040
Dean McNamee <dean@gmail.com>
2011-03-19 18:13:04 +0000

    New deps for node upgrade.

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -1,4 +1,4 @@
 rm -rf deps
-curl -O http://cloud.github.com/downloads/deanm/plask/deps.tar.bz2
-tar jxvf deps.tar.bz2
-rm deps.tar.bz2
+curl -O http://cloud.github.com/downloads/deanm/plask/deps2.tar.bz2
+tar jxvf deps2.tar.bz2
+rm deps2.tar.bz2
