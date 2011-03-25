5cf80a29a75bfd73ae8c2270393b91c419809320
Dean McNamee <dean@gmail.com>
2011-03-25 15:25:00 +0000

    Example 002, pulsing circle.

---
+++ examples/002_hello_circle_pulse.js
@@ -0,0 +1,40 @@
+//
+// A simple Plask example, drawing a pulsing red circle on a gray background.
+//
+
+var plask = require('plask');
+
+plask.simpleWindow({
+  init: function() {
+    var canvas = this.canvas, paint = this.paint;
+
+    // The call to framerate() sets up how often the screen should be redrawn.
+    // By default, the value is 0, and the screen is only drawn once during
+    // initialization, and then any time when redraw() is called.  The value
+    // is the number of frames that should be drawn every second.
+    this.framerate(30);
+
+    paint.setStyle(paint.kFillStyle);
+    paint.setFlags(paint.kAntiAliasFlag);
+    paint.setColor(80, 0, 0, 255);
+  },
+
+  draw: function() {
+    var canvas = this.canvas, paint = this.paint;
+
+    canvas.drawColor(230, 230, 230, 255);
+
+    // The frametime property is automatically computed by simpleWindow(), and
+    // contains the number of seconds since the window was created.
+    var t = Math.sin(this.frametime) * 0.5 + 0.5;
+
+    // The lerp function performs a linear interpolation between two values.
+    // By cycling the value of the interpolation between 0 and 1, the radius
+    // will cycle between 50 and 100 pixels.
+    var radius = plask.lerp(50, 100, t);
+
+    // The canvas object contains its width and height, which can be used to
+    // compute the x and y coordinates of the center of the canvas.
+    canvas.drawCircle(paint, canvas.width/2, canvas.height/2, radius);
+  }
+});
