1efaf84fbfe508045529f1023e698f7b871b5239
Dean McNamee <dean@gmail.com>
2011-04-07 00:57:04 +0530

    Two examples of keyboard handling.

---
+++ examples/007_hello_circle_keyboard.js
@@ -0,0 +1,48 @@
+//
+// A simple Plask example, drawing a red circle on a gray background centered
+// where the mouse is moved.
+//
+
+var plask = require('plask');
+
+plask.simpleWindow({
+  // Introducing the settings object, which allows you to set things like the
+  // window width and height, title, window type and configuration, etc.
+  settings: {
+    width: 400,
+    height: 300,
+    title: "Move with the AWSD keys."
+  },
+  
+  init: function() {
+    var canvas = this.canvas, paint = this.paint;
+
+    this.center = {x: canvas.width/2, y: canvas.height/2};
+
+    this.on('keyDown', function(e) {
+      // The |str| property on the key event is a string of the unicode
+      // translation of the key.  Take bigger steps while holding shift.
+      switch (e.str) {
+        case 'w': this.center.y -= 1; break;
+        case 'W': this.center.y -= 10; break;
+        case 's': this.center.y += 1; break;
+        case 'S': this.center.y += 10; break;
+        case 'a': this.center.x -= 1; break;
+        case 'A': this.center.x -= 10; break;
+        case 'd': this.center.x += 1; break;
+        case 'D': this.center.x += 10; break;
+      }
+      this.redraw();
+    });
+
+    paint.setStyle(paint.kFillStyle);
+    paint.setFlags(paint.kAntiAliasFlag);
+    paint.setColor(80, 0, 0, 255);
+  },
+
+  draw: function() {
+    var canvas = this.canvas, paint = this.paint;
+    canvas.drawColor(230, 230, 230, 255);
+    canvas.drawCircle(paint, this.center.x, this.center.y, 100);
+  }
+});
---
+++ examples/008_hello_circle_keyboard_arrows.js
@@ -0,0 +1,64 @@
+//
+// A simple Plask example, drawing a red circle on a gray background centered
+// where the mouse is moved.
+//
+
+var plask = require('plask');
+
+plask.simpleWindow({
+  // Introducing the settings object, which allows you to set things like the
+  // window width and height, title, window type and configuration, etc.
+  settings: {
+    width: 400,
+    height: 300,
+    title: "Move with the AWSD or arrow keys."
+  },
+  
+  init: function() {
+    var canvas = this.canvas, paint = this.paint;
+
+    this.center = {x: canvas.width/2, y: canvas.height/2};
+
+    this.on('keyDown', function(e) {
+      // NOTE: The function keys are mapped into the private unicode area
+      // 0xf700 by AppKit, see NSUpArrowFunctionKey and friends.
+      switch (e.str) {
+        case '\uf700': case 'w': case 'W':
+          this.center.y -= e.shift ? 10 : 1; break;
+        case '\uf701': case 's': case 'S':
+          this.center.y += e.shift ? 10 : 1; break;
+        case '\uf702': case 'a': case 'A':
+          this.center.x -= e.shift ? 10 : 1; break;
+        case '\uf703': case 'd': case 'D':
+          this.center.x += e.shift ? 10 : 1; break;
+      }
+
+      // An alternative to the unicode |str| property is the low-level key
+      // code.  This has different semantics, for example, an 'A' key with
+      // shift held is the same code as without shift held.  Key codes are raw
+      // identifiers of the keyboard, and generally you should use the |str|
+      // property instead to account for keyboard layout and language.
+      //
+      // The keyCode version looks something like:
+      //
+      // switch (e.keyCode) {
+      //   case 126: case 13: this.center.y -= e.shift ? 10 : 1; break;
+      //   case 125: case 1: this.center.y += e.shift ? 10 : 1; break;
+      //   case 123: case 0: this.center.x -= e.shift ? 10 : 1; break;
+      //   case 124: case 2: this.center.x += e.shift ? 10 : 1; break;
+      // }
+      
+      this.redraw();
+    });
+
+    paint.setStyle(paint.kFillStyle);
+    paint.setFlags(paint.kAntiAliasFlag);
+    paint.setColor(80, 0, 0, 255);
+  },
+
+  draw: function() {
+    var canvas = this.canvas, paint = this.paint;
+    canvas.drawColor(230, 230, 230, 255);
+    canvas.drawCircle(paint, this.center.x, this.center.y, 100);
+  }
+});
