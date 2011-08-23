59ea104991ac4f6fc8b21e274bcfbcba25bbc0d5
Dean McNamee <dean@gmail.com>
2011-08-23 17:57:13 +0200

    Update dependencies, this brings Skia to r2139.

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -1,4 +1,4 @@
 rm -rf deps
-curl -O http://cloud.github.com/downloads/deanm/plask/deps4.tar.bz2
-tar jxvf deps4.tar.bz2
-rm deps4.tar.bz2
+curl -O http://cloud.github.com/downloads/deanm/plask/deps5.tar.bz2
+tar jxvf deps5.tar.bz2
+rm deps5.tar.bz2
