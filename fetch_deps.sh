84bb20babcd81e2fb4820f00169b1591ce470e41
Dean McNamee <dean@gmail.com>
2012-12-23 01:48:44 +0100

    Update to new node deps (now hosted on google code).

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -1,4 +1,4 @@
 rm -rf deps
-curl -O http://cloud.github.com/downloads/deanm/plask/deps6.tar.bz2
-tar jxvf deps6.tar.bz2
-rm deps6.tar.bz2
+curl -O http://plaskdeps.googlecode.com/files/deps7.tar.bz2
+tar jxvf deps7.tar.bz2
+rm deps7.tar.bz2
