29e416be5a83cafaeb8fd2c4e3808948e8ada5a0
Dean McNamee <dean@gmail.com>
2014-10-18 18:54:36 +0200

    Update to deps_v3_002 for Skia build d12af70.

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -1,5 +1,5 @@
-NAME=deps_v3_001
+NAME=deps_v3_002
 rm -rf deps
 curl -L -O https://googledrive.com/host/0B4M1ew30nMnnMFo2WVFrVDliU1U/$NAME.tar.bz2
 tar jxvf $NAME.tar.bz2
-rm $NAME.tar.bz2
+#rm $NAME.tar.bz2
